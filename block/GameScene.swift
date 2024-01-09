b2673dbf0d6df30df2d5b1665a31b1aea341dd17
tunogya <20251942+tunogya@users.noreply.github.com>
2024-01-09 23:10:11 +0800

    update

--- block/GameScene.swift
+++ block/GameScene.swift
@@ -12,14 +12,12 @@ import UIKit
 class GameScene: SKScene {
     
     private var asstNode : SKShapeNode?
-    private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
         self.backgroundColor = SKColor.black
         
         // Create shape node to use during mouse interaction
         let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: 0)
         self.asstNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: 0)
         
         if let asstNode = self.asstNode {
@@ -27,15 +25,6 @@ class GameScene: SKScene {
             asstNode.physicsBody = SKPhysicsBody(rectangleOf: asstNode.frame.size)
             asstNode.physicsBody?.isDynamic = false
             self.addChild(asstNode)
-        }
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
         }
     }
     
@@ -54,49 +43,29 @@ class GameScene: SKScene {
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            self.addChild(n)
-            moveAsstNode(toPoint: pos)
-        }
+        moveAsstNode(toPoint: pos)
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            self.addChild(n)
-            moveAsstNode(toPoint: pos)
-        }
+        moveAsstNode(toPoint: pos)
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            self.addChild(n)
-            moveAsstNode(toPoint: pos)
-        }
+        moveAsstNode(toPoint: pos)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let feedbackGenerator = UIImpactFeedbackGenerator(style: .medium)
-            feedbackGenerator.prepare()
-            feedbackGenerator.impactOccurred()
-
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let feedbackGenerator = UIImpactFeedbackGenerator(style: .soft)
-            feedbackGenerator.prepare()
-            feedbackGenerator.impactOccurred()
-        
         for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let feedbackGenerator = UIImpactFeedbackGenerator(style: .medium)
-            feedbackGenerator.prepare()
-            feedbackGenerator.impactOccurred()
+//        let feedbackGenerator = UIImpactFeedbackGenerator(style: .medium)
+//            feedbackGenerator.prepare()
+//            feedbackGenerator.impactOccurred()
         
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
