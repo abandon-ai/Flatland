72d486197574dff1ebe25580a53ed0928dbd43ed
tunogya <20251942+tunogya@users.noreply.github.com>
2024-01-13 15:08:48 +0800

    update

--- Flatland/GameScene.swift
+++ Flatland/GameScene.swift
@@ -7,73 +7,92 @@ class GameScene: SKScene {
     private var circleNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
-        // add background
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = CGRect(origin: .zero, size: size)
-        gradientLayer.colors = [UIColor(hex: "#6775F2").cgColor, UIColor(hex: "#D585F6").cgColor]
-        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
-        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
-        
-        UIGraphicsBeginImageContext(gradientLayer.bounds.size)
-        gradientLayer.render(in: UIGraphicsGetCurrentContext()!)
-        let backgroundImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-
-        let backgroundTexture = SKTexture(image: backgroundImage!)
-
-        let backgroundNode = SKSpriteNode(texture: backgroundTexture)
-        backgroundNode.size = self.size
-        backgroundNode.position = CGPoint(x: 0, y: 0)
+        // Background
+        let backgroundNode = createBackgroundNode(size: self.size, colors: ["#6775F2", "#D585F6"], startPoint: CGPoint(x: 0.5, y: 0), endPoint: CGPoint(x: 0.5, y: 1))
+        backgroundNode.position = .zero
         backgroundNode.zPosition = -1
         self.addChild(backgroundNode)
         
-        // circleNode
-        self.circleNode = SKShapeNode.init(rectOf: CGSize.init(width: 144, height: 144), cornerRadius: 144)
-        if let spinnyNode = self.circleNode {
-            spinnyNode.lineWidth = 4
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                                SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        // Circle Node
+        createCircleNode()
         
-        // squareBodyNode
+        // Square Node
+        let squareNode = createSquareNode(size: CGSize(width: 96, height: 96), blurRadius: 10.0, colors: ["#3D70E5", "#CEFEEC"], bloomIntensity: 1.0, bloomRadius: 10.0)
+        self.addChild(squareNode)
+    }
+
+    private func createBackgroundNode(size: CGSize, colors: [String], startPoint: CGPoint, endPoint: CGPoint) -> SKSpriteNode {
+        let gradientLayer = createGradientLayer(frame: CGRect(origin: .zero, size: size), colors: colors, startPoint: startPoint, endPoint: endPoint)
+        let backgroundImage = image(from: gradientLayer)
+        let backgroundTexture = SKTexture(image: backgroundImage)
+        return SKSpriteNode(texture: backgroundTexture)
+    }
+
+    private func createCircleNode() {
+        self.circleNode = SKShapeNode(rectOf: CGSize(width: 144, height: 144), cornerRadius: 144)
+        circleNode?.lineWidth = 4
+        circleNode?.run(SKAction.sequence([
+            SKAction.wait(forDuration: 0.5),
+            SKAction.fadeOut(withDuration: 0.5),
+            SKAction.removeFromParent()
+        ]))
+    }
+
+    private func createSquareNode(size: CGSize, blurRadius: CGFloat, colors: [String], bloomIntensity: CGFloat, bloomRadius: CGFloat) -> SKNode {
+        let squareBodyNode = createSquareBodyNode(size: size, blurRadius: blurRadius, colors: colors)
+        let squareStrokeNode = createSquareStrokeNode(size: size, bloomIntensity: bloomIntensity, bloomRadius: bloomRadius)
+        
+        let squareNode = SKNode()
+        squareNode.addChild(squareBodyNode)
+        squareNode.addChild(squareStrokeNode)
+        return squareNode
+    }
+
+    private func createSquareBodyNode(size: CGSize, blurRadius: CGFloat, colors: [String]) -> SKEffectNode {
         let squareBodyNode = SKEffectNode()
         squareBodyNode.shouldRasterize = true
-        squareBodyNode.filter = CIFilter(name: "CIGaussianBlur", parameters: ["inputRadius": 10.0])
+        squareBodyNode.filter = CIFilter(name: "CIGaussianBlur", parameters: ["inputRadius": blurRadius])
         
-        let gradientLayer2 = CAGradientLayer()
-        gradientLayer2.frame = CGRect(origin: .zero, size: CGSize(width: 96, height: 96))
-        gradientLayer2.colors = [UIColor(hex: "#3D70E5").cgColor, UIColor(hex: "#CEFEEC").cgColor]
-        gradientLayer2.startPoint = CGPoint(x: 0.0, y: 0.5)
-        gradientLayer2.endPoint = CGPoint(x: 1.0, y: 0.5)
+        let gradientLayer = createGradientLayer(frame: CGRect(origin: .zero, size: size), colors: colors, startPoint: CGPoint(x: 0.0, y: 0.5), endPoint: CGPoint(x: 1.0, y: 0.5))
+        let gradientImage = image(from: gradientLayer)
         
-        UIGraphicsBeginImageContext(gradientLayer2.bounds.size)
-        gradientLayer2.render(in: UIGraphicsGetCurrentContext()!)
-        let gradientImage2 = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        let squareSprite = SKSpriteNode(texture: SKTexture(image: gradientImage2!))
+        let squareSprite = SKSpriteNode(texture: SKTexture(image: gradientImage))
         squareBodyNode.addChild(squareSprite)
-        // squareStrokeNode
+        return squareBodyNode
+    }
+
+    private func createSquareStrokeNode(size: CGSize, bloomIntensity: CGFloat, bloomRadius: CGFloat) -> SKEffectNode {
         let squareStrokeNode = SKEffectNode()
         
         let bloomFilter = CIFilter(name: "CIBloom")!
-        bloomFilter.setValue(1.0, forKey: kCIInputIntensityKey)
-        bloomFilter.setValue(10.0, forKey: kCIInputRadiusKey)
+        bloomFilter.setValue(bloomIntensity, forKey: kCIInputIntensityKey)
+        bloomFilter.setValue(bloomRadius, forKey: kCIInputRadiusKey)
         
         squareStrokeNode.filter = bloomFilter
         squareStrokeNode.shouldEnableEffects = true
-        let borderNode = SKShapeNode(rectOf: CGSize(width: 96, height: 96))
+        let borderNode = SKShapeNode(rectOf: size)
         borderNode.strokeColor = SKColor.white
         borderNode.lineWidth = 4
         borderNode.fillColor = SKColor.clear
         squareStrokeNode.addChild(borderNode)
-        // squareNode
-        self.squareNode = SKNode()
-        self.squareNode?.addChild(squareBodyNode)
-        self.squareNode?.addChild(squareStrokeNode)
-        
-        self.addChild(self.squareNode!)
+        return squareStrokeNode
+    }
+
+    private func createGradientLayer(frame: CGRect, colors: [String], startPoint: CGPoint, endPoint: CGPoint) -> CAGradientLayer {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = frame
+        gradientLayer.colors = colors.map { UIColor(hex: $0).cgColor }
+        gradientLayer.startPoint = startPoint
+        gradientLayer.endPoint = endPoint
+        return gradientLayer
+    }
+
+    private func image(from layer: CALayer) -> UIImage {
+        UIGraphicsBeginImageContext(layer.bounds.size)
+        layer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image!
     }
     
     func moveSquareNode(toPoint pos: CGPoint) {
