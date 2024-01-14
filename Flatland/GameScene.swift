f8c6486d4edaf3039f46c93b219d0ba4f475a330
tunogya <20251942+tunogya@users.noreply.github.com>
2024-01-14 16:14:05 +0800

    update

--- Flatland/GameScene.swift
+++ Flatland/GameScene.swift
@@ -3,15 +3,69 @@ import GameplayKit
 
 class GameScene: SKScene {
     var squareNode = Square()
-    private var circleNode: Circle!
+    private var circleNode: SKSpriteNode!
     
     override func didMove(to view: SKView) {
         let backgroundNode = Background(size: self.size)
         self.addChild(backgroundNode)
         
-        circleNode = Circle()
         squareNode.name = "Angle"
         self.addChild(squareNode)
+        
+        let circleStrokeNode = createBloomStrokeNode(size: CGSize(width: 96, height: 96), lineWidth: 4, radius: 48, bloomIntensity: 2.0, bloomRadius: 10)
+        circleNode = SKSpriteNode()
+        circleNode.addChild(circleStrokeNode)
+        circleNode.run(SKAction.sequence([
+            SKAction.wait(forDuration: 0.5),
+            SKAction.fadeOut(withDuration: 0.5),
+            SKAction.removeFromParent()
+        ]))
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            if let n = self.circleNode?.copy() as! SKSpriteNode? {
+                n.position = location
+                self.addChild(n)
+            }
+            if squareNode.contains(location) {
+                let feedbackGenerator = UIImpactFeedbackGenerator(style: .heavy)
+                    feedbackGenerator.prepare()
+                    feedbackGenerator.impactOccurred()
+            }
+        }
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            if let n = self.circleNode?.copy() as! SKSpriteNode? {
+                n.position = location
+                self.addChild(n)
+            }
+        }
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+        for touch in touches {
+            let location = touch.location(in: self)
+            if let n = self.circleNode?.copy() as! SKSpriteNode? {
+                n.position = location
+                self.addChild(n)
+            }
+        }
+    }
+
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            if let n = self.circleNode?.copy() as! SKSpriteNode? {
+                n.position = location
+                self.addChild(n)
+            }
+        }
     }
     
     override func update(_ currentTime: TimeInterval) {
